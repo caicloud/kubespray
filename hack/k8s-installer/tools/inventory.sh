e3cba63edf692f3a040416eb8d645905634d9ff5
muzi502 <lixianxu@caicloud.io>
2021-01-21 17:13:32 +0800

    chore(hack): adjust inventory path to absolute path

--- hack/k8s-installer/tools/inventory.sh
+++ hack/k8s-installer/tools/inventory.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # bash inventory.sh <master_ips> <node_ips> <etcd_ips>
-# like this "bash inventory.sh 1.1.1.1-3 1.1.1.4-8 1.1.1.1-3"
-
+# like this "bash tools/inventory.sh 1.1.1.1-3 1.1.1.4-8 1.1.1.1-3"
+DEPLOY_ROOT=$(cd $(dirname "${BASH_SOURCE}")/ && pwd -P)
+INVENTORY="${DEPLOY_ROOT/tools/}inventory"
 # match and split correct IPv4 to list
 match_ips() {
-    INPUT_IPS=$*
-    IPS="${INPUT_IPS}"
-    if ! echo ${IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}-[[:digit:]]{1,3}' > /dev/null; then
-        IPS="$(echo ${IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' | tr '\n' ' ')"
+    local INPUT_IPS=$*
+    local IPS=""
+    if ! echo ${INPUT_IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}-[[:digit:]]{1,3}' > /dev/null; then
+        IPS="$(echo ${INPUT_IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' | tr '\n' ' ')"
     else
-        ip_prefix="$(echo ${IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}-[[:digit:]]{1,3}' | cut -d '.' -f1-3)"
-        ip_suffix="$(echo ${IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}-[[:digit:]]{1,3}' | cut -d '.' -f4 | tr '-' ' ')"
+        ip_prefix="$(echo ${INPUT_IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}-[[:digit:]]{1,3}' | cut -d '.' -f1-3)"
+        ip_suffix="$(echo ${INPUT_IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}-[[:digit:]]{1,3}' | cut -d '.' -f4 | tr '-' ' ')"
         for suffix in $(seq ${ip_suffix}); do IPS="${IPS} ${ip_prefix}.${suffix}"; done
     fi
     echo ${IPS} | egrep --only-matching -E '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' | tr '\n' ' '
@@ -18,7 +19,7 @@ match_ips() {
 
 # generate inventory template
 gen_inventory_template() {
-    cat > inventory <<EOF
+    cat > ${INVENTORY} <<EOF
 [all:vars]
 ansible_port=22
 ansible_user=root
@@ -49,7 +50,6 @@ gen_inventory() {
     k8s_master_ips=$(match_ips $(echo $1))
     k8s_node_ips=$(match_ips $(echo $2))
     k8s_etcd_ips=$(match_ips $(echo $3))
-    k8s_etcd_ips=${k8s_etcd_ips:=${k8s_master_ips}}
     gen_inventory_template
 
     local count=0
@@ -57,10 +57,10 @@ gen_inventory() {
         count=$(($count+1))
         hostname="kube-master-${count}"
         ansible_ssh_host="ansible_host=${ip}"
-        sed -i- "/\[all\]/a  ${hostname} ${ansible_ssh_host}" inventory
-        sed -i- "/\[kube-master\]/a ${hostname}" inventory
-        sed -i- "/\[kube-node\]/a ${hostname}" inventory
-        if [[ "${ip}" =~ "${k8s_etcd_ips}" ]]; then sed -i- "/\[etcd\]/a ${hostname}" inventory; fi
+        sed -i "/\[all\]/a  ${hostname} ${ansible_ssh_host}" ${INVENTORY}
+        sed -i "/\[kube-master\]/a ${hostname}" ${INVENTORY}
+        sed -i "/\[kube-node\]/a ${hostname}" ${INVENTORY}
+        if echo "${k8s_etcd_ips}" | grep "${ip}"; then sed -i "/\[etcd\]/a ${hostname}" ${INVENTORY}; fi
     done
 
     local count=0
@@ -68,21 +68,26 @@ gen_inventory() {
         count=$(($count+1))
         hostname="kube-node-${count}"
         ansible_ssh_host="ansible_host=${ip}"
-        sed -i- "/\[all\]/a ${hostname} ${ansible_ssh_host}" inventory
-        sed -i- "/\[kube-node\]/a ${hostname}" inventory
+        sed -i "/\[all\]/a ${hostname} ${ansible_ssh_host}" ${INVENTORY}
+        sed -i "/\[kube-node\]/a ${hostname}" ${INVENTORY}
     done
 
-    if [ "${k8s_etcd_ips}" == "${k8s_master_ips}" ]; then
+    if ! echo "${k8s_master_ips}" | grep "${k8s_etcd_ips}"; then
         local count=0
-        for ip in $(echo ${etcd_ips}); do
+        for ip in $(echo ${k8s_etcd_ips}); do
             count=$(($count+1))
             hostname="kube-etcd-${count}"
             ansible_ssh_host="ansible_host=${ip}"
-            sed -i- "/\[all\]/a ${hostname} ${ansible_ssh_host}" inventory
-            sed -i- "/\[etcd\]/a ${hostname}" inventory
+            sed -i "/\[all\]/a ${hostname} ${ansible_ssh_host}" ${INVENTORY}
+            sed -i "/\[etcd\]/a ${hostname}" ${INVENTORY}
         done
     fi
 }
 
-gen_inventory $1 $2 $3
-vi inventory
+MASTER_IPS=$1
+NODE_IPS=$2
+ETCD_IPS=$3
+ETCD_IPS=${ETCD_IPS:=$1}
+
+gen_inventory ${MASTER_IPS} ${NODE_IPS} ${ETCD_IPS}
+vi ${INVENTORY}
